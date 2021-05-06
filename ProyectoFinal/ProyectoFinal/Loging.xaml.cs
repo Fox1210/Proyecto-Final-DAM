6d4cab4feaa42c2ee2c0bf0cc94879727f3eb15b
Pablo Gómez Meléndez <pablogomezmelendez@gmail.com>
2021-05-06 18:45:58 +0200

    Prueba de navegacion entre ventanas

--- ProyectoFinal/ProyectoFinal/Loging.xaml.cs
+++ ProyectoFinal/ProyectoFinal/Loging.xaml.cs
@@ -1,60 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using Windows.UI.Popups;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
-
-
-// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace ProyectoFinal
 {
     /// <summary>
-    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Loging : Page
     {
-        private ConnectionBBDD BASE_DATOS = new ConnectionBBDD();
-       
-
-        public MainPage()
+        public Loging()
         {
             this.InitializeComponent();
         }
 
-        private async void accederLoging(object sender, RoutedEventArgs e)
+        private void volverBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (UserTextBox.Text == String.Empty | UserPasswordBox.Password == String.Empty) 
-            {
-                var msg = new MessageDialog("Los campos USUARIO y CONTRASEÑA deben estar rellenos");
-                await msg.ShowAsync();
-            }
-            else {
-                string lista = "resultado: \n";
-                //consulta a la vase de datos
-                Usuario usuario = BASE_DATOS.logingUser(UserTextBox.Text, UserPasswordBox.Password);
-                if (usuario.isEmpty())
-                {
-                    var msg = new MessageDialog("El usuario o la contraseña son invalidos");
-                    await msg.ShowAsync();
-                }
-                else
-                {
-                    App.user = usuario;
-                    //if (usuario.Type == 1)
-                    //{
-                    //    this.Frame.Navigate(typeof(PaginaInicioAdmin));
-                    //}
-                    //else
-                    //{
-                    //    this.Frame.Navigate(typeof(PaginaInicioUser));
-                    //}
-                    //resultado.Text = usuario.ToString();
-                }
-                
-            }
-            
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
---
+++ ProyectoFinal/ProyectoFinal/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace ProyectoFinal
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ConnectionBBDD BASE_DATOS = new ConnectionBBDD();
+       
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void accederLoging(object sender, RoutedEventArgs e)
+        {
+            if (UserTextBox.Text == String.Empty | UserPasswordBox.Password == String.Empty) 
+            {
+                var msg = new MessageDialog("Los campos USUARIO y CONTRASEÑA deben estar rellenos");
+                await msg.ShowAsync();
+            }
+            else {
+                string lista = "resultado: \n";
+                //consulta a la vase de datos
+                Usuario usuario = BASE_DATOS.logingUser(UserTextBox.Text, UserPasswordBox.Password);
+                if (usuario.isEmpty())
+                {
+                    var msg = new MessageDialog("El usuario o la contraseña son invalidos");
+                    await msg.ShowAsync();
+                }
+                else
+                {
+                    App.user = usuario;
+                    if (usuario.Type == 1)
+                    {
+                        this.Frame.Navigate(typeof(Loging));
+                        //this.Frame.Navigate(typeof(PaginaInicioAdmin));
+                    }
+                    //else
+                    //{
+                    //    this.Frame.Navigate(typeof(PaginaInicioUser));
+                    //}
+                    //resultado.Text = usuario.ToString();
+                }
+                
+            }
+            
+        }
+    }
+}
